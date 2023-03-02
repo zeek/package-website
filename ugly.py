4f313c6a440a7fab00cc0039af40df30a55a1c09
montaquila <akiel@akiel.org>
2023-03-01 17:29:43 -0700

    testing format workflow with zeek-bot user and email

--- ugly.py
+++ ugly.py
@@ -2,11 +2,13 @@ import numpy    as np
 import pandas as    pd
 
 
-
 def adder(a, b):
     """
     this should be realigned
     """
     c = a + b print(    a +  b)
+    
+    b = c+ c
+    print(c +a)
 
 

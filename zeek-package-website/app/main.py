86d7628ec69b20fc6e81e6c28005a89e3b4c78a9
David Knight <davidmlknight@gmail.com>
2023-03-23 12:10:52 -0700

    got to a stable point of viewing on localhost with uvicorn. Commented out code leads to errors when running with modules not being found - a solution will be found if needed or code will be removed if it remains unused.

--- zeek-package-website/app/main.py
+++ zeek-package-website/app/main.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-from api.routes.api import router as api_router
-from core.events import create_start_app_handler
-from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
-
-
-def get_application() -> FastAPI:
-    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
-    application.include_router(api_router, prefix=API_PREFIX)
-    pre_load = False
-    if pre_load:
-        application.add_event_handler("startup", create_start_app_handler(application))
-    return application
+# from api.routes.api import router as api_router
+# from core.events import create_start_app_handler
+# from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 
 
-app = get_application()
+# def get_application() -> FastAPI:
+#    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
+#    application.include_router(api_router, prefix=API_PREFIX)
+#    pre_load = False
+#    if pre_load:
+#        application.add_event_handler("startup", create_start_app_handler(application))
+#    return application
+
+
+# app = get_application()
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    data = {
+        "text": "hi"
+    }
+    return {"data": data}
+
+
+@app.get("/page/{page_name}")
+async def page(page_name: str):
+    data = {
+        "page": page_name
+    }
+    return {"data": data}

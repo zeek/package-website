5d05af7bb531107f0e1e524a379b997009b9235c
cjb873 <cjb873@nau.edu>
2023-03-27 13:33:53 -0700

    Update to BM25+

--- search/search.py
+++ search/search.py
@@ -1,17 +1,6 @@
 from numpy import log as ln
 
 
-def rank(documents: [], query: str) -> []:
-    scores = []
-    query = query.lower().split()
-    idfs = get_idfs(documents, query)
-    avgdl = get_avgdl(documents)
-    for document in documents:
-        scores.append(score(document, query, avgdl, idfs))
-
-    return scores
-
-
 def get_avgdl(documents: []) -> int:
     avgdl = 0
 
@@ -21,34 +10,12 @@ def get_avgdl(documents: []) -> int:
     return avgdl / len(documents)
 
 
-def score(document: [], query: [], avgdl: int, idfs: dict) -> int:
-    score = 0
-
-    for term in query:
-        score += score_helper(document, term, avgdl, idfs)
-
-    return score
-
-
-def score_helper(document: str, term: str, avgdl: int, idfs: dict) -> int:
-
-    frequency = get_frequency(document, term)
-    idf = idfs.get(term)
-    k1 = 1.6  # k1 is a free value anywhere from 1.2 to 2
-    b = 0.75  # b is a free value that is commonly 0.75
-
-    score = ((frequency * (k1 + 1)) /
-             (frequency + k1 * (1 - b + b * len(document) / avgdl)))
-
-    return idf * score
-
-
 def get_frequency(document: str, term: str) -> int:
 
     frequency = 0
 
     for word in document:
-        if word.lower() == term:
+        if term in word:
             frequency += 1
 
     return frequency
@@ -67,17 +34,53 @@ def get_idfs_helper(documents: [], term: str) -> int:
     document_frequency = 0
 
     for document in documents:
-        if term in document:
-            document_frequency += 1
+        for word in document:
+            if term in word.lower():
+                document_frequency += 1
 
     return ln(((len(documents) - document_frequency + 0.5) /
                (document_frequency + 0.5)) + 1)
 
 
+def rank(documents: [], query: str) -> []:
+    scores = []
+    query = query.lower().split()
+    idfs = get_idfs(documents, query)
+    avgdl = get_avgdl(documents)
+
+    for document in documents:
+        scores.append(score(document, query, avgdl, idfs))
+
+    return scores
+
+
+def score(document: [], query: [], avgdl: int, idfs: dict) -> int:
+    score = 0
+
+    for term in query:
+        score += score_helper(document, term, avgdl, idfs)
+
+    return score
+
+
+def score_helper(document: str, term: str, avgdl: int, idfs: dict) -> int:
+
+    frequency = get_frequency(document, term)
+    idf = idfs.get(term)
+    k1 = 1.0  # k1 is a free variable anywhere from 1.2 to 2
+    b = 0.75  # b is a free variable that is commonly 0.75
+    delta = 1.0  # delta is a free variable that is commonly 1.0
+    score = ((frequency * (k1 + 1)) /
+             (frequency + k1 * (1 - b + b * (len(document) / avgdl)))) + delta
+
+    return idf * score
+
+
 def search(query: str) -> dict:
 
     documents = []
-    document_names = ["example1.txt", "example2.txt", "example3.txt"]
+    document_names = ["bro-ja3.txt", "bro-sysmon.txt", "cve-2020-16898.txt",
+                      "got_zoom.txt", "hassh.txt", "ja3.txt"]
 
     for name in document_names:
         text_file = open(name, "r")
@@ -92,7 +95,7 @@ def search(query: str) -> dict:
 
 
 def main():
-    query = "yellow cat"
+    query = "ssh"
 
     print(f"The query is: {query}\n")
 

354a611614f73dbf2608374dcd9585aeeed95098
David Knight <davidmlknight@gmail.com>
2023-03-28 17:25:32 -0700

    mounted static folder to allow for use of static files such as images and css

--- zeek-package-website/app/main.py
+++ zeek-package-website/app/main.py
@@ -1,25 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-# from api.routes.api import router as api_router
-# from core.events import create_start_app_handler
-# from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
-
-
-# def get_application() -> FastAPI:
-#    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
-#    application.include_router(api_router, prefix=API_PREFIX)
-#    pre_load = False
-#    if pre_load:
-#        application.add_event_handler("startup", create_start_app_handler(application))
-#    return application
-
-
-# app = get_application()
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)

bc4332c28c6cf902439a90edf2d7f8fd44694e4d
montaquila <akiel@akiel.org>
2023-03-29 20:27:59 -0700

    progress with parser. oopifying the methods

--- zeek-package-website/app/api/parser.py
+++ zeek-package-website/app/api/parser.py
@@ -1,36 +1,55 @@
 import re
 
 
-def parse(file):
-    with open(file, 'r') as f:
-        contents = f.read()
+class Parse:
+    def __init__(self, file):
+        with open(file, 'r') as f:
+            contents = f.read()
 
-# Find all matches of the regular expression in the file contents
-    matches = re.findall(
-        r'\[[^/^][^[]*?\/[^[]*?\][^[]*',
-        contents,
-        flags=re.DOTALL)
+        section_headers = self.find_section_headers(contents)
+        descriptions = self.find_descriptions(contents)
 
-# Loop over each match and extract the desired fields
-    for match in matches:
-        # Extract the section header
-        section_header = re.search(r'\[[^/^].*?\/[^[]*?\]', match).group(0)
+        # Combine section headers and descriptions into tuples
+        #self.results = list(zip(section_headers, descriptions))
 
-        # Extract the description
-        description_match = re.search(
-            r'^description\s*=\s*(.+)', match, flags=re.MULTILINE)
-        if description_match:
-            description = description_match.group(1)
+    def find_section_headers(self, file_contents):
+        section_headers = re.findall(
+            r'\[[^/^][^[]*?\/[^[]*?\]',
+            file_contents,
+            flags=re.DOTALL)
+        return section_headers
 
-            # Print the section header and description
-            print(section_header)
-            print(description)
-            print()  # Print a blank line between entries
+    def find_descriptions(self, file_contents):
+        matches = re.findall(
+            r'\[[^/^][^[]*?\/[^[]*?\][^[]*',
+            file_contents,
+            flags=re.DOTALL)
+        
+        # empty list of descs to append
+        descriptions = []
+
+        for match in matches:
+            description_match = re.search(
+                r'^description\s*=\s*(.+)', match, flags=re.MULTILINE)
+            if description_match:
+                description = description_match.group(1)
+                descriptions.append(description)
+        return descriptions
 
 
 def main():
     file = 'aggregate.meta'
-    parse(file)
+    parse = Parse(file)
+
+    package_num = 1
+
+    for section_header, description in parse.results:
+        print(f"Package {package_num}:")
+        print(section_header)
+        print(description)
+        print()
+
+        package_num += 1
 
 
 if __name__ == '__main__':

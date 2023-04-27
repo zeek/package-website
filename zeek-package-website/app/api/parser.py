42787757455db85a66290ea4820bb5d1c21a9d5c
cjb873 <cjb873@nau.edu>
2023-04-27 13:22:41 -0700

    Remove smelly code in get_readme

--- zeek-package-website/app/api/parser.py
+++ zeek-package-website/app/api/parser.py
@@ -91,7 +91,7 @@ class Parse(object):
             # section_count to keep track of # of packages
             self.section_count += 1
 
-        return self.data_dict
+        return self.pkg_dict
 
 
 
@@ -177,12 +177,13 @@ class Parse(object):
 
     def dump(self):
 
-        for item in self.data_dict.items():
+        for item in self.pkg_dict.items():
             name = item[0].split("/")[1]
             name = name.strip("]")
             with open(f"search/json_files/{name}.json", "w+",
                       encoding="utf-8") as outfile:
                 outfile.write(json.dumps(item[1]))
+
 
     def get_readme(self) -> str:
         """
@@ -201,42 +202,25 @@ class Parse(object):
         name = name.removesuffix(".git")
         request_url = f"https://raw.githubusercontent.com/{name}/master"
 
-        get_request = requests.get(f"{request_url}/README.md")
+        # if packages have other forms of readme add to this list
+        filenames = ["README.md", "readme.md", "README", "readme", "Readme.md"
+                     "Readme", "README.rst", "Readme.rst", "readme.rst"]
+        counter = 1
+
+        readme_ext = filenames[0]
+        get_request = requests.get(f"{request_url}/{readme_ext}")
+
+        while not get_request.ok and counter < len(filenames):
+            readme_ext = filenames[counter]
+            get_request = requests.get(f"{request_url}/{readme_ext}")
+            counter += 1
+
+        # check for special case, as one package uses gitlab
+        if "https://gitlab.com" in name:
+            get_request = requests.get(f"{name}/-/raw/master/README.md")
 
         if not get_request.ok:
-            get_request = requests.get(f"{request_url}/readme.md")
-
-            if not get_request.ok:
-                get_request = requests.get(f"{request_url}/README")
-
-                if not get_request.ok:
-                    get_request = requests.get(f"{request_url}/readme")
-
-                    if not get_request.ok:
-                        get_request = requests.get(f"{request_url}/Readme.md")
-
-                        if not get_request.ok:
-                            get_request = requests.get(f"{request_url}/Readme")
-
-                            if not get_request.ok:
-                                get_request = requests.get(
-                                    f"{request_url}/README.rst")
-
-                                if not get_request.ok:
-                                    get_request = requests.get(
-                                        f"{request_url}/Readme.rst")
-
-                                    if not get_request.ok:
-                                        get_request = requests.get(
-                                            f"{request_url}/readme.rst")
-
-                                        if not get_request.ok:
-                                            if "https://gitlab.com" in name:
-                                                get_request = requests.get(
-                                                    f"{name}/-/raw/master/README.md")
-
-                                            if not get_request.ok:
-                                                return None
+            return None
 
         return get_request.content.decode("utf-8")
 

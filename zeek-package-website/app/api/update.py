44a7abe36664e47d283d42150807ef0c460c25cc
cjb873 <cjb873@nau.edu>
2023-04-30 18:31:32 -0700

    Create update.py

---
+++ zeek-package-website/app/api/update.py
@@ -0,0 +1,21 @@
+import subprocess
+from parser import Parse
+
+
+def main():
+    std_out = subprocess.run(["git", "pull", "origin", "demo-updates"],
+                             capture_output=True).stdout
+    file = 'aggregate.meta'
+
+    if not std_out == b'Already up to date.\n':
+        print("Parsing")
+        parse = Parse(file)
+        parse.parse_data()
+        parse.dump
+        print("Parsed")
+    else:
+        print(std_out.decode())
+
+
+if __name__ == "__main__":
+    main()

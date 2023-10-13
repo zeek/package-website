8d8c313058d472cebbd3812f6f04373ea994b8d9
nkc53 <91169337+nkc53@users.noreply.github.com>
2023-10-12 18:59:09 -0700

    Update main.py

--- zeek-package-website/app/main.py
+++ zeek-package-website/app/main.py
@@ -34,6 +34,7 @@ async def package(request: Request, package_name: str):
         "package_info": result,
         "package_readme": readme
     }
+<<<<<<< HEAD
     # send users back to home page if package does not exist
     if result is not None:
         return templates.TemplateResponse("package-info.html", {"request": request, "data": data})
@@ -54,3 +55,9 @@ async def search(request: Request, query: str = Form(...)):
 @repeat_every(seconds=60 * 4)
 async def update_helper():
     update("aggregate.meta")
+=======
+    return templates.TemplateResponse("package-info.html", {"request": request, "data": data})
+
+@app.get("/about", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})

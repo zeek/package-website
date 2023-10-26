31be34964af6294eae2ab3adae6121228e9c70e0
cjb873 <cjb873@nau.edu>
2023-10-26 15:40:52 -0700

    Add searching for code within field

--- zeek-package-website/app/api/readme_scraper.py
+++ zeek-package-website/app/api/readme_scraper.py
@@ -21,11 +21,12 @@ def load_packages() -> []:
                     for field in missing:
                         item = get_field(package_json["readme"], field)
                         if item is not None:
-                            package_json[field] = item.group()
+                            package_json[field] = item
                             changes += 1
 
             if changes > 0:
-                with open(f"{filepath}/{filename}", "w+", encoding="utf-8") as file:
+                with open(f"{filepath}/{filename}",
+                          "w+", encoding="utf-8") as file:
                     json.dump(package_json, file)
 
 
@@ -43,4 +44,16 @@ def get_field(readme: str, field: str) -> str:
 
     search_term = f"{fields[field]}"
 
-    return re.search(f'(?<=# {search_term})(.*)(?=\n)', readme.lower())
+    field = re.search(f'(?<=# {search_term})(.*)(?=\n)', readme.lower())
+    term = None
+
+    if field is not None:
+        term = re.search(r'(?<=`)(.*)(?=`)', field.group())
+
+        if term is not None:
+            return term.group()
+
+        return field.group()
+
+    return None
+

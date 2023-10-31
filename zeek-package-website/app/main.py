c806e5895d78c224421d262dafa9dbe23dbfea06
David Knight <75804651+davidknight00@users.noreply.github.com>
2023-10-30 21:03:40 -0700

    Update main.py
    proper formating

--- zeek-package-website/app/main.py
+++ zeek-package-website/app/main.py
@@ -15,6 +15,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})

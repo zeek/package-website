5f82990f53c6827d38a593eb3f0688f2fc0178d3
akielaries <akiel@akiel.org>
2023-12-03 15:12:49 -0700

    updating unit tests for parser. fixes #50

--- zeek-package-website/tests/test_parser.py
+++ zeek-package-website/tests/test_parser.py
@@ -1,8 +1,30 @@
+import os
+import json
+import pytest
 from app.api.parser import Parse
 
+def test_pkg_data():
+    sample_file = 'tests/sample1.meta'
 
-parse = Parse("app/api/aggregate.meta")
-parse.parse_data()
-print("done")
-print("test pass?")
-print("test pass?")
+    parser = Parse(sample_file)
+    result = parser.parse_data()
+
+    # test package from sample1.meta
+    pkg1 = parser.pkg_dict["[0xxon/cve-2020-13777]"]
+
+    assert pkg1["description"] == '"Test script for CVE-2020-13777. Please read Readme."'
+    assert pkg1["tags"] is None
+    assert pkg1["version"] == "main"
+    #print(pkg1)
+    #print(pkg1["depends"])
+    assert pkg1["depends"] == ['zkg >=2.0', 'zeek >=4.0.0']
+    assert pkg1["test_cmd"] == "cd testing && btest -d"
+    assert pkg1["url"] == "https://github.com/0xxon/cve-2020-13777"
+    assert pkg1["script_dir"] == "scripts"
+    assert pkg1["plugin_dir"] is None
+
+    pkg2 = result["[0xxon/zeek-network-statistics]"]
+
+
+
+
